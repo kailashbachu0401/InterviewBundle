75321248e7571e3d09d17bd33f1214ead7b7864b
bkailashh <bkailashh@eventbrite.com>
2026-02-06 18:56:51 +0530

    improv

--- SystemDesign/FastAPIDemo/main.py
+++ SystemDesign/FastAPIDemo/main.py
@@ -2,9 +2,13 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi import status
+from fastapi import Depends
+from fastapi import Query
+from fastapi.responses import JSONResponse
 
 app = FastAPI(title="Event API Demo", description="A simple event API", version="0.1.0")
 
+# Pydantic Model
 class Event(BaseModel):
     id: int
     name: str
@@ -29,25 +33,42 @@ def increment():
 
 # Path Param
 @app.get("/event/{id}")
-def get_event(id: str):
-    return {
-        "event_id": id,
-    }
-
-# Query Param
-# http://localhost:8000/event?id=1&name=sample
-@app.get("/event")
-def get_event(id: int, name: str):
+def get_event(id: str, name: str): # name is query param
+    # raise EventNotFoundException(id)
     return {
         "event_id": id,
         "event_name": name
     }
 
-# Req body
-# 202 Accepted - used for Async operations
-@app.post("/event", status_code=status.HTTP_202_ACCEPTED)
-def create_event(event: Event):
+# Query Param
+# http://localhost:8000/event?id=1&name=sample
+@app.get("/event")
+def get_event(event: Event = Depends()): # Pydantic model in fun params is treated as request body and expects JSON, Depends() tells FastApi to serialize the model from query params
     return {
         "event_id": event.id,
         "event_name": event.name
     }
+
+# Req body
+# 202 Accepted - used for Async operations
+@app.post("/event", status_code=status.HTTP_202_ACCEPTED)
+def create_event(event: Event, extra: str): # extra is query param
+    return {
+        "event_id": event.id,
+        "event_name": event.name,
+        "extra": extra
+    }
+
+class EventNotFoundException(Exception):
+    def __init__(self, event_id: str):
+        self.event_id = event_id
+
+@app.exception_handler(EventNotFoundException)
+def event_not_found_exc_handler(req, exc: EventNotFoundException):
+    return JSONResponse(
+        status_code = status.HTTP_404_NOT_FOUND,
+        content = {
+            "error_code": "EVENT_NOT_FOUND",
+            "error_message": f"event {exc.event_id} not found"
+        }
+    )
